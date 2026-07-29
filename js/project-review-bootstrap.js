d74c86ef9a6ad883ce2616a9d7ae29642ca96994
juanperez238421-cpu <juanperez238421@gmail.com>
2026-07-29 07:45:36 -0500

    feat: open the exact activity selected during interview

--- js/project-review-bootstrap.js
+++ js/project-review-bootstrap.js
@@ -1,6 +1,8 @@
 import "./project-review-local.js";
 
-const requestedStudentId = new URLSearchParams(window.location.search).get("student");
+const params = new URLSearchParams(window.location.search);
+const requestedStudentId = params.get("student");
+const requestedEntry = params.get("entry");
 
 if (requestedStudentId) {
   let attempts = 0;
@@ -10,6 +12,22 @@ if (requestedStudentId) {
     if (button) {
       button.click();
       window.clearInterval(timer);
+      if (requestedEntry) {
+        let entryAttempts = 0;
+        const entryTimer = window.setInterval(() => {
+          entryAttempts += 1;
+          const select = document.querySelector("#entryFileSelect");
+          const option = select ? [...select.options].find((item) => item.value === requestedEntry) : null;
+          if (select && option) {
+            select.value = requestedEntry;
+            select.dispatchEvent(new Event("change", { bubbles: true }));
+            document.querySelector("#runSelectedButton")?.click();
+            window.clearInterval(entryTimer);
+          } else if (entryAttempts >= 50) {
+            window.clearInterval(entryTimer);
+          }
+        }, 160);
+      }
     } else if (attempts >= 40) {
       window.clearInterval(timer);
     }
